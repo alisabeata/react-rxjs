be76277fd1e78180774c9b6dac4f117fbca5debd
Alisa Vasilenko <alisa314159@gmail.com>
2024-04-20 16:03:12 +0200

    add ObserveButton, pipe, fromEvent, subscription

--- src/components/ObserveButton.js
+++ src/components/ObserveButton.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useRef } from 'react'
 import { fromEvent } from 'rxjs'
+import { throttleTime, map } from 'rxjs/operators'
 
 const ObserveButton = () => {
   const buttonRef = useRef(null)
 
   useEffect(() => {
     if (buttonRef.current) {
-      const buttonClicks = fromEvent(buttonRef.current, 'click')
+      // Use pipe to apply multiple operators
+      const buttonClicks = fromEvent(buttonRef.current, 'click').pipe(
+        throttleTime(1000), // Throttle clicks to once per second, allows to init function by click once per second
+        map((event) => event.timeStamp), // wrap each instance, get timeStamp val from the event object
+      )
 
-      const subscription = buttonClicks.subscribe(() => {
-        console.log('Button was clicked!')
+      // Subsctibe
+      const subscription = buttonClicks.subscribe((data) => {
+        console.log(data)
       })
 
       return () => subscription.unsubscribe()
